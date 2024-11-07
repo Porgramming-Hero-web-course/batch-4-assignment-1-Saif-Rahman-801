a5d9561cf617d6a9d3de2b591af0b397c9c5b60e
Saif-Rahman-801 <saifurs102@gmail.com>
2024-11-07 21:34:16 +0600

    Problem 01 solved

---
+++ problem-1.ts
@@ -0,0 +1,15 @@
+{
+  //
+
+  const sumArray = (arrNums: number[]): number => {
+    return arrNums.reduce(
+      (previousVal, presentVal) => previousVal + presentVal,
+      0
+    );
+  };
+
+  // const returnValue = sumArray([1, 2, 3, 4, 5]);
+  // console.log(returnValue);
+
+  //
+}
