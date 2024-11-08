42299526c962963994292beda45eae28525cef2c
Saif-Rahman-801 <saifurs102@gmail.com>
2024-11-08 18:31:57 +0600

    problem-4 solved

---
+++ problem-4.ts
@@ -0,0 +1,34 @@
+{
+  //
+  interface Circle {
+    shape: "circle";
+    radius: number;
+  }
+
+  interface Rectangle {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  }
+
+  type Shape = Circle | Rectangle;
+
+  const calculateShapeArea = (shapeInfo: Shape): number => {
+    if (shapeInfo.shape === "circle") {
+      return Math.PI * shapeInfo.radius ** 2;
+    } else {
+      return shapeInfo.width * shapeInfo.height;
+    }
+  };
+  
+//   console.log(calculateShapeArea({ shape: "circle", radius: 5 }));
+//   console.log(
+//     calculateShapeArea({
+//       shape: "rectangle",
+//       width: 4,
+//       height: 6,
+//     })
+//   );
+
+  //
+}
